c717984e7b4628eeb9e6bc30a8a33e89715114b2
JM Arenas <jm.arenas@interleukin.co>
2022-10-12 17:48:01 +0800

    Added confirmable to user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,9 +2,10 @@ class User < ApplicationRecord
   acts_as_token_authenticatable
   
   # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  # , :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+         :confirmable
 
   has_many :accounts, foreign_key: :owner_id
 end
