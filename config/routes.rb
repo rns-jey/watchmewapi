75cd98e09e87638cda514c66293938691dd24478
JM Arenas <jm.arenas@interleukin.co>
2022-10-24 21:02:16 +0800

    Nested contacts resource only show index

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,7 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   namespace :v1, defaults: { format: :json } do
     resources :accounts, only: %i[create update] do
-      resources :contacts
+      resources :contacts, only: [:index]
     end
     resource :sessions, only: %i[create destroy]
     resources :users, only: [:create]
