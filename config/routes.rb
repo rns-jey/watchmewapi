91fb4a6dbe34ef3cb6561c287670cba3e3af87cf
JM Arenas <jm.arenas@interleukin.co>
2022-10-24 17:18:27 +0800

    Built accounts route and nest contacts inside

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,13 @@
+# frozen_string_literal: true
+
 Rails.application.routes.draw do
   devise_for :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   namespace :v1, defaults: { format: :json } do
-    resources :contacts
-    resource :sessions, only: [:create, :destroy]
+    resources :accounts, only: %i[create update] do
+      resources :contacts
+    end
+    resource :sessions, only: %i[create destroy]
     resources :users, only: [:create]
   end
 end
