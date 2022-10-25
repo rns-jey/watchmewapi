015b83dc6606898adb294383fb5f0680abe240fa
JM Arenas <jm.arenas@interleukin.co>
2022-10-25 12:24:36 +0800

    Nested sessions under V1 module

--- app/controllers/v1/sessions_controller.rb
+++ app/controllers/v1/sessions_controller.rb
@@ -1,29 +1,34 @@
-class V1::SessionsController < ApplicationController
-  def show
-    current_user ? head(:ok) : head(:unauthorized)
-  end
+# frozen_string_literal: true
 
-  def create
-    @user = User.where(email: params[:email]).first
-
-    if @user&.valid_password?(params[:password])
-      render :create, status: :created
-    else
-      head(:unauthorized)
+module V1
+  # Sessions Endpoints
+  class SessionsController < ApplicationController
+    def show
+      current_user ? head(:ok) : head(:unauthorized)
     end
-  end
 
-  def destroy
-    if nilify_token && current_user.save
-      head(:ok)
-    else
-      head(:unauthorized)
+    def create
+      @user = User.where(email: params[:email]).first
+
+      if @user&.valid_password?(params[:password])
+        render :create, status: :created
+      else
+        head(:unauthorized)
+      end
     end
-  end
 
-  private
+    def destroy
+      if nilify_token && current_user.save
+        head(:ok)
+      else
+        head(:unauthorized)
+      end
+    end
+
+    private
 
     def nilify_token
       current_user&.authentication_token = nil
     end
+  end
 end
